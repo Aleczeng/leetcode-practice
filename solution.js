b17a494d7bc9130ac4d2f1466adf4dc1a0a6cc0e
AlecZeng <zxh93321@gmail.com>
2019-07-12 22:15:43 +1200

    # 14

--- solution.js
+++ solution.js
@@ -102,4 +102,25 @@ const romanToInt_2 = s => {
     return result
 };
 
-console.log(romanToInt_2('MCDLXXVIIIII'));
+// console.log(romanToInt_2('MCDLXXVIIIII'));
+
+// 14. Longest Common Prefix
+
+const longestCommonPrefix = strs => {
+    if (strs == null || strs.length < 1) {
+        return "";
+    }
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        console.log(strs[i].indexOf(prefix));
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (prefix.length < 1) {
+                return "";
+            }
+        }
+    }
+    return prefix;
+};
+
+// console.log(longestCommonPrefix(["c","acc","ccc"]));
